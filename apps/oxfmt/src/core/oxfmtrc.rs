c9741f3b38dadb4967e866febbcd96b2a43802e1
nilptr <nilptr.js@gmail.com>
2026-01-07 00:07:40 +0800

    refactor(oxfmt): reverse dependency

--- apps/oxfmt/src/core/oxfmtrc.rs
+++ apps/oxfmt/src/core/oxfmtrc.rs
@@ -5,8 +5,8 @@ use serde_json::Value;
 use oxc_formatter::{
     ArrowParentheses, AttributePosition, BracketSameLine, BracketSpacing,
     EmbeddedLanguageFormatting, Expand, FormatOptions, IndentStyle, IndentWidth, LineEnding,
-    LineWidth, QuoteProperties, QuoteStyle, Semicolons, SortImportsOptions, SortOrder,
-    TailwindcssOptions, TrailingCommas,
+    LineWidth, NormalizedCustomGroupDefinition, QuoteProperties, QuoteStyle, Semicolons,
+    SortImportsOptions, SortOrder, TailwindcssOptions, TrailingCommas,
 };
 
 /// Configuration options for the Oxfmt.
@@ -715,7 +715,7 @@ impl Oxfmtrc {
                 sort_imports.groups = v;
             }
             if let Some(v) = config.custom_groups {
-                sort_imports.custom_groups = v;
+                sort_imports.custom_groups = v.into_iter().map(std::convert::Into::into).collect();
             }
 
             // `partition_by_newline: true` and `newlines_between: true` cannot be used together
